47cb16e8198d5c9ff6fb86a1871ecf5fcfc8f109
Westin Fishel <wfishel22@jcu.edu>
2022-05-09 19:18:41 -0400

    literal final

--- script.js
+++ script.js
@@ -9,17 +9,24 @@ const WINNING_COMBINATIONS = [
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
 
 startGame()
 
+restartButton.addEventListener('click', startGame)
+
 function startGame() {
     circleTurn = false
     cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS)
+        cell.classList.remove(CIRCLE_CLASS)
+        cell.removeEventListener('click',handleClick)
         cell.addEventListener('click', handleClick, { once: true })
     })
     setBoardHoverClass()
+    //winningMessageElement.classList.remove('show')
 }
 
 function handleClick(e) {
@@ -38,7 +45,7 @@ function handleClick(e) {
 
 function endGame(draw) {
     if (draw) {
-        winningMessageElement.innerText = 'Draw!'
+        winningMessageTextElement.innerText = 'Draw!'
     } else {
         winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
     }
@@ -46,7 +53,7 @@ function endGame(draw) {
 }
 
 function isDraw() {
-    return cellElements.every(cell => {
+    return [...cellElements].every(cell => {
         return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
     })
 }
